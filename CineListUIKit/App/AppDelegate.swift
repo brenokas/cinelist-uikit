cec510abd578fe4e3c657920a8f17a9756822ac8
Breno <brenofreitas28f@gmail.com>
2026-09-21 13:50:39 -0300

    feat: adiciona integracao com analytics do firebase

--- CineListUIKit/App/AppDelegate.swift
+++ CineListUIKit/App/AppDelegate.swift
@@ -8,11 +8,13 @@
 import UIKit
 import FirebaseCore
 import FirebaseAuth
+import FirebaseAnalytics
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         FirebaseApp.configure()
+        Analytics.setAnalyticsCollectionEnabled(true)
         // Override point for customization after application launch.
         return true
     }
